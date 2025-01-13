8e5a225963829388a0fabf0affd181bfa061c957
Zsoltcsizmadia <163414210+Zsoltcsizmadia@users.noreply.github.com>
2025-01-13 09:30:19 +0100

    Add project files.

---
+++ 2025-01-13 Kalapcsvetes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2025_01_13_Kalapcsvetes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
